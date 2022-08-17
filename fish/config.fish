b467981ec15bcd5720c6d781062cc7e9ddae1828
sirmax <erotourtes@gmail.com>
2022-08-17 13:22:14 +0300

    major bugs fixed
    added neovim commits
    chnaged HDMI-1 to HDMI1
    changed drum mode rofi
    man reader neovim
    fish vi prompt

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,15 @@
 #chsh -s $(which fish)
 
 set -gx EDITOR nvim
+set -gx MANPAGER "nvim +Man!"
+
+
+alias t="tmux"
+alias ta="tmux attach -t"
+alias tk="tmux kill-session -t"
+alias v="nvim"
+
+function mkfile; mkdir -p $argv[1] && touch $argv[1]/$argv[2]; end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
--- i3/wallpaper.sh
+++ i3/wallpaper.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 #run chmod u+x wallpaper
+if [ "$(pgrep -c wallpaper.sh)" -gt "1" ]; then
+  exit 1;
+fi
+
 while true
 do
     feh --bg-fill --randomize /home/sirmax/Fedora_HDD/Pictures/wallpapers/* 
-    sleep 2h
+    echo running;
+    sleep 1h
 done
