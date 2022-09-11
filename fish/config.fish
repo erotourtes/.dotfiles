0588eefff9ed5fbfbd057b1064562762fd026537
sirmax <erotourtes@gmail.com>
2022-09-11 20:53:02 +0300

    fish and my scripts

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,7 @@ alias tk="tmux kill-session -t"
 alias vim="nvim"
 alias l="exa --group-directories-first -1"
 
+bind -M insert \cl "accept-autosuggestion"
 
 function mkfile; mkdir -p $argv[1] && touch $argv[1]/$argv[2]; end
 function pd; cd ~/Fedora_HDD/projects/; end
@@ -19,5 +20,6 @@ function pd; cd ~/Fedora_HDD/projects/; end
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
+    # fish_hybrid_key_bindings 
 end
 
---
+++ i3/scripts/open-clipboard-image.fish
@@ -0,0 +1,3 @@
+#!/bin/fish
+
+xclip -selection clipboard -target image/png -out > ~/Fedora_HDD/Pictures/screenshots/clipboard.png && sxiv ~/Fedora_HDD/Pictures/screenshots/clipboard.png
