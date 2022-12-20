9029eac64da4351ff198b806189d7286010ab5d6
sirmax <erotourtes@gmail.com>
2022-12-20 12:31:47 +0200

    fix(open-clipboard-image): multiple photos now opening

--- i3/scripts/open-clipboard-image.fish
+++ i3/scripts/open-clipboard-image.fish
@@ -1,3 +1,15 @@
 #!/bin/fish
 
-xclip -selection clipboard -target image/png -out > ~/Fedora_HDD/Pictures/screenshots/clipboard.png && sxiv ~/Fedora_HDD/Pictures/screenshots/clipboard.png
+set picture ~/Fedora_HDD/Pictures/screenshots/
+set lastScreen (l $picture | rg clipboard |rg -o "\d")[-1]
+set newScreen (math $lastScreen + 1)
+if not test -n "$newScreen"
+  echo "File do not exist, setting newScreen to 0"
+  set newScreen 0
+end
+
+set picture $picture'clipboard'$newScreen'.png'
+
+xclip -selection clipboard -target image/png -out > $picture && sxiv $picture 
+
+rm $picture
