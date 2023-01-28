17ed7f301f51de35e5f54eff31c7523eb3d56afa
Max Sir <erotourtes@gmail.com>
2023-01-28 11:44:55 +0200

    feat(fish): final version of fdd and fdf scripts

--- fish/functions/fdd.fish
+++ fish/functions/fdd.fish
@@ -1,16 +1,17 @@
 function fdd
-  set -l path ~/Files/Documents/projects/
+  set -l path ~/Documents/projects/
   set -l options "h/help" 
   set options $options "a/all" 
   set options $options (fish_opt -s p -l path --required-val)
   argparse $options -- $argv
   if set -q _flag_help; echo "Default path:" $path \n$options; return 0; end
   if set -q _flag_path; set path $_flag_path; end
+  if test -n "$argv[1]"; and test -e "$argv[1]"; set path $argv[1]; end
 
   echo $path
 
   if set -q _flag_all
-    cd (fd --base-directory $path -H -I -t d | fzf)
+    cd (fd --base-directory $path -H -t d | fzf)
   else
     cd $path/(fd --base-directory  $path -t d | string split -n $path | fzf)
   end
--- fish/functions/fdf.fish
+++ fish/functions/fdf.fish
@@ -4,6 +4,7 @@ function fdf
   set -l path "./"
   set -l options "h/help" 
   set options $options "o/open-file" 
+  set options $options "a/all" 
   set options $options (fish_opt -s p -l path --required-val)
   argparse $options -- $argv
   if set -q _flag_help; echo "Default path:" $path \n$options; return 0; end
@@ -11,7 +12,11 @@ function fdf
   if test -n "$argv[1]"; and test -e "$argv[1]"; set path $argv[1]; end
 
   # set file (find $path -not -path '*/.*'  -type f -iname '*' -not -iname '*.jpg' -not -iname '*.png' | fzf)
-  set file $path(fd --base-directory $path -t f -E "*.png" -E "*.jpg" | fzf)
+  if set -q _flag_all
+    set file $path(fd --base-directory $path -t f | fzf)
+  else 
+    set file $path(fd --base-directory $path -t f -E "*.png" -E "*.jpg" | fzf)
+  end
 
   if set -q _flag_open_file;
     if test -e "$file"; xdg-open $file; end
