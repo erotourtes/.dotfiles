7bbfa986ef688131c7a00465bed3125b2aa96408
Max Sir <erotourtes@gmail.com>
2023-01-29 20:37:53 +0200

    check commit

--- fish/functions/add.fish
+++ fish/functions/add.fish
@@ -1,10 +1,15 @@
-function add
+function add -d "Create a new file or directory"
     set -l options "h/help" "o/open"
     argparse $options -- $argv
     if set -q _flag_help; echo $options; return 0; end
  
 
     set -l path $argv[1]
+
+    if test -z $path
+        echo "path is null"
+        return 1
+    end
 
     if test (string sub -s -1 $path) = "/"
         mkdir -p $path
