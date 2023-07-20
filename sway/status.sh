b740ead8bc807c823356ef7690ba6d33604ed3e5
Max Siryk <erotourtes@gmail.com>
2023-07-20 15:21:20 +0300

    updated sway

--- sway/status.sh
+++ sway/status.sh
@@ -5,7 +5,8 @@
 # The abbreviated weekday (e.g., "Sat"), followed by the ISO-formatted date
 # like 2018-10-06 and the time (e.g., 14:01). Check `man date` on how to format
 # time and date.
-date_formatted=$(date "+%Y-%m-%d %I:%M:%S %p")
+date_formatted=$(date "+%Y-%m-%d")
+time=$(date "+%I:%M:%S %p")
 
 # "upower --enumerate | grep 'BAT'" gets the battery name (e.g.,
 # "/org/freedesktop/UPower/devices/battery_BAT0") from all power devices.
@@ -15,10 +16,8 @@ date_formatted=$(date "+%Y-%m-%d %I:%M:%S %p")
 # identifiers. i3 and sway convert the newline between battery state and
 # the charge percentage automatically to a space, producing a result like
 # "charging 59%" or "fully-charged 100%".
-battery_info=$(upower --show-info $(upower --enumerate |\
-grep 'BAT') |\
-egrep "state|percentage" |\
-awk '{print $2}')
+battery_charge=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "percentage" | awk '{print $2}')
+battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}')
 
 # "amixer -M" gets the mapped volume for evaluating the percentage which
 # is more natural to the human ear according to "man amixer".
@@ -37,4 +36,23 @@ brightness=$(brightnessctl | grep -oE "[0-9]+%")
 # Separators: \| ❘ ❙ ❚
 # Misc: 🐧 💎 💻 💡 ⭐ 📁 ↑ ↓ ✉ ✅ ❎
 
-echo "  󰕾"$audio_volume"  󰃚"$brightness"  "$battery_info"  "$date_formatted
+network=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
+# interface_easyname grabs the "old" interface name before systemd renamed it
+interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
+# ping=$(ping -c 1 www.google.es | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
+
+if ! [ $network ]
+then
+   network_active="⛔"
+else
+   network_active="⇆"
+fi
+
+if [ $battery_status = "discharging" ];
+then
+    battery_pluggedin='⚡'
+else
+    battery_pluggedin=''
+fi
+
+echo "$time | 󰕾 $audio_volume | 󰃚 $brightness | $battery_pluggedin  $battery_charge |  $date_formatted | $network_active $interface_easyname"
