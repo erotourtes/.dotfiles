c1cb6c75ee37aa90f5b16dc6ac589ef20425c4f5
Max Siryk <erotourtes@gmail.com>
2023-07-21 19:06:22 +0300

    updated sway

--- sway/status.sh
+++ sway/status.sh
@@ -5,7 +5,7 @@
 # The abbreviated weekday (e.g., "Sat"), followed by the ISO-formatted date
 # like 2018-10-06 and the time (e.g., 14:01). Check `man date` on how to format
 # time and date.
-date_formatted=$(date "+%Y-%m-%d")
+date_formatted=$(date "+%Y-%m-%d %a")
 time=$(date "+%I:%M:%S %p")
 
 # "upower --enumerate | grep 'BAT'" gets the battery name (e.g.,
@@ -39,7 +39,7 @@ brightness=$(brightnessctl | grep -oE "[0-9]+%")
 network=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
 # interface_easyname grabs the "old" interface name before systemd renamed it
 interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
-# ping=$(ping -c 1 www.google.es | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
+ping=$(ping -c 1 www.google.es | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
 
 if ! [ $network ]
 then
@@ -55,4 +55,4 @@ else
     battery_pluggedin=''
 fi
 
-echo "$time | 󰕾 $audio_volume | 󰃚 $brightness | $battery_pluggedin  $battery_charge |  $date_formatted | $network_active $interface_easyname"
+echo "$time | 󰕾 $audio_volume | 󰃚 $brightness | $battery_pluggedin  $battery_charge |  $date_formatted | $network_active ($ping)ms $interface_easyname"
