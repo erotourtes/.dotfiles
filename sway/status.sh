691ec616e1d5492c8199b9a98fb932273f85921d
erotourtes <erotourtes@gmail.com>
2023-11-15 20:35:43 +0200

    changed status

--- sway/status.sh
+++ sway/status.sh
@@ -40,7 +40,6 @@ brightness=$(brightnessctl | grep -oE "[0-9]+%")
 network=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
 # interface_easyname grabs the "old" interface name before systemd renamed it
 interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
-ping=$(ping -c 1 www.google.es | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
 
 if ! [ $network ]
 then
@@ -72,4 +71,6 @@ fi
 
 media_title=$(playerctl metadata title)
 
-echo "$time | $audio_volume_icon $audio_volume | 󰃚 $brightness | $battery_pluggedin  $battery_charge |  $date_formatted | $network_active ($ping)ms $interface_easyname"
+micro=$(amixer get Capture | grep -q '\[off\]' && echo "󰍭" || echo "󰍬")
+
+echo "$time | $micro | $audio_volume_icon $audio_volume | 󰃚 $brightness | $battery_pluggedin  $battery_charge |  $date_formatted | $network_active $interface_easyname"
