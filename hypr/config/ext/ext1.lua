96545e986fd73194402ae1d0fdb3bf26067c4fc7
erotourtes <erotourtes@gmail.com>
2026-07-16 07:31:43 +0200

    feat: wsync

--- hypr/config/ext/ext1.lua
+++ hypr/config/ext/ext1.lua
@@ -1,6 +1,7 @@
 ---@type fun(ctx: HyprConfigContext): nil
 local function run(ctx)
     hl.env("SSH_AUTH_SOCK", "/run/user/1000/keyring/ssh")
+    hl.env("_JAVA_AWT_WM_NONREPARENTING", "1")
 
     hl.on("hyprland.start", function()
         hl.exec_cmd("gnome-keyring-daemon --start --components=secrets,ssh")
@@ -10,6 +11,9 @@ local function run(ctx)
         hl.exec_cmd("/opt/google/chrome/google-chrome --profile-directory=Default --app-id=kjbdgfilnfhdoflbpgamdcdgpehopbep")
         hl.exec_cmd(ctx.terminal .. " fish -c \"~/Documents/work/wingtra/scripts/tmux.fish\"")
     end)
+
+
+    hl.window_rule({ match = { class = "^scrcpy$" }, workspace = "5 silent" })
 end
 
 local M = {
